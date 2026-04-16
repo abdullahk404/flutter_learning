581a280273e47b258fc4125945d9305d9016125c
Abdullah Khalid <muhammadabdullahkhalid622@gmail.com>
2026-04-16 17:59:19 +0500

    Dashboard Screen In progress

--- lib/my_first_project/my_practice2.dart
+++ lib/my_first_project/my_practice2.dart
@@ -1,31 +1,73 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main( )=> runApp(MaterialApp(
- debugShowCheckedModeBanner:false,
-home: SystemDashboard(),
-));
+void main() => runApp(
+  MaterialApp(debugShowCheckedModeBanner: false, home: SystemDashboard()),
+);
+
 // Stateful widget because we want to toggle a status
-class SystemDashboard extends StatefulWidget{
- @override
- SystemDashboardState createState()=> SystemDashboardState();
+class SystemDashboard extends StatefulWidget {
+  @override
+  SystemDashboardState createState() => SystemDashboardState();
 }
 
 class SystemDashboardState extends State<SystemDashboard> {
- @override
- Widget build (BuildContext context ){
-  return Scaffold(
-   backgroundColor:Colors.grey[200],
-   body: Column (
-    children: [
-     Container(
-      height: 200,
-      width:double.infinity,
-      color: Colors.blueGrey,
-     )
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      body: Column(
+        children: [
+          Stack(
+            alignment: Alignment.bottomCenter,
+            clipBehavior: Clip.none, // Allows profile pic to hang over the edge
+            children: [
+              Container(
+                height: 200,
+                width: double.infinity,
+                color: Colors.blueGrey,
+                child: Center(
+                  child: Text(
+                    "ABDULLAH",
+                    style: TextStyle(color: Colors.white ,letterSpacing: 2, fontWeight: FontWeight.bold),
+                  ),
 
-    ],
-   )
-  );
- }
+                ),
+              ),
+              Positioned(
+                  bottom:-50,
+                  child: CircleAvatar(
+                    radius: 60,
+                    backgroundColor: Colors.white,
+                    child:Icon( Icons.person,size:80,color: Colors.grey,),
+                  ))
+            ],
+          ),
+          SizedBox(height: 60,),
+          Text("ELIN TECHNOLOGIES ",style: TextStyle(fontSize: 22,fontWeight: FontWeight.bold),),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.location_on,size:16,color: Colors.red,),
+              Text("Islamabad",style: TextStyle(fontWeight: FontWeight.bold),)
+            ],
+          ),
+          Padding(padding: EdgeInsets.all(20),
+              child: Container(
+                padding: EdgeInsets.all(15),
+                decoration: BoxDecoration(color: Colors.white,borderRadius: BorderRadius.circular(5)
+
+                ),
+              ),
+
+          ),
+         
+          
+        ],
+        
+        
+      ),
+      
+    );
+  }
 }
