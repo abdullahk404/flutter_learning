278c8e55dcab6aa860fd9ed6562ae8bad2eee299
Abdullah Khalid <muhammadabdullahkhalid622@gmail.com>
2026-05-11 12:43:43 +0500

    Drawer

--- lib/my_first_project/revission.dart
+++ lib/my_first_project/revission.dart
@@ -11,13 +11,88 @@ class MyPracticeScreen extends StatelessWidget {
     return DefaultTabController(
       length: 2,
       child: Scaffold(
+        // Drawer
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children:[
+              DrawerHeader(
+                  decoration: BoxDecoration(color: Colors.blue.shade200),
+                  child:
+              Column(
+                 crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  CircleAvatar(radius: 30,
+
+                    backgroundColor: Colors.black,
+                    child: Padding(padding: EdgeInsetsGeometry.all(8.3),
+                      child: Image.network('https://cdn-icons-png.flaticon.com/512/3046/3046121.png'
+                      , fit:BoxFit.contain,),
+
+
+                    ),
+
+                   ),
+                  Text("Tiktok",style: TextStyle(fontSize:25))
+
+                ],
+
+              )
+              ),
+          // Menu Items
+          ListTile(
+            leading: const Icon(Icons.home),
+              title: const Text("Home"),
+              onTap:()=> Navigator.pop(context)
+      ),
+        ListTile(
+          leading: const Icon(Icons.language),
+          title: const Text("Language"),
+          onTap: () => Navigator.pop(context),
+        ),
+              ListTile(
+                leading: const Icon(Icons. notifications),
+                title: const Text("Notifications"),
+                onTap: () => Navigator.pop(context),
+              ),
+              ListTile(
+                leading: const Icon(Icons.privacy_tip),
+                title: const Text("Privacy"),
+                onTap: () => Navigator.pop(context),
+              ),
+              ListTile(
+                leading: const Icon(Icons.share),
+                title: const Text("Share"),
+                onTap: () => Navigator.pop(context),
+              ),
+              ListTile(
+                leading: const Icon(Icons.stars),
+                title: const Text("Rate Us"),
+                onTap: () => Navigator.pop(context),
+              ),
+              ListTile(
+                leading: const Icon(Icons.download),
+                title: const Text("Downloads"),
+                onTap: () => Navigator.pop(context),
+              ),
+
+
+
+
+
+
+
+            ],
+
+          ),
+        ),
         // App Bar
         appBar: AppBar(
           // Left side leading section
-          leading: IconButton(
-            onPressed: () => print("Mane Tapped"),
-            icon: const Icon(Icons.menu),
-          ),
+          // leading: IconButton(
+          //   onPressed: () => print("Mane Tapped"),
+          //   icon: const Icon(Icons.menu),
+          // ),
           // The Middle
           title: const Text("My Apps"),
           centerTitle: true,
@@ -36,7 +111,10 @@ class MyPracticeScreen extends StatelessWidget {
           // The Style
           backgroundColor: Colors.blue.shade200,
           elevation: 4,
+          // TabBar
           bottom: const TabBar(
+            unselectedLabelColor: Colors.blueGrey,
+            labelColor: Colors.black,
             tabs: [
               Tab(icon: Icon(Icons.camera_alt), text: "Camera"),
               Tab(icon: Icon(Icons.photo_library),text: "Gallery", ),
